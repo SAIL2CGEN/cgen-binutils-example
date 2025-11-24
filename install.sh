2d54fa6d102fd0267aae152d76834c0670c5f8f2
pdn002 <pdn002@ucsd.edu>
2025-11-24 00:22:36 -0800

    updated install.sh script with correct tool versions

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-wget ftp://sourceware.org/pub/binutils/releases/binutils-2.25.tar.gz
-tar -xzf binutils-2.25.tar.gz
+wget https://sourceware.org/pub/binutils/releases/binutils-2.31.1.tar.bz2
+tar -xzf binutils-2.31.tar.gz
 rm binutils-2.25.tar.gz
-wget ftp://sourceware.org/pub/cgen/releases/cgen-1.1.tar.gz
-tar -xzf cgen-1.1.tar.gz
+wget https://sourceware.org/pub/cgen/snapshots/cgen-20180601.tar.bz2
+mkdir cgen-20180601
+tar -xvjf cgen-20180601.tar.bz2 -C ./cgen-20180601
 rm cgen-1.1.tar.gz
