b8c5494bf4e2d83f87d7236077468a6b95781fb5
pdn002 <pdn002@ucsd.edu>
2025-11-24 20:56:37 -0800

    updated instal script to match previous commit changes

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget https://sourceware.org/pub/binutils/releases/binutils-2.31.1.tar.bz2
+wget https://sourceware.org/pub/binutils/releases/binutils-2.31.tar.gz
 tar -xzf binutils-2.31.tar.gz
-rm binutils-2.25.tar.gz
+rm binutils-2.31.tar.gz
 wget https://sourceware.org/pub/cgen/snapshots/cgen-20180601.tar.bz2
 mkdir cgen-20180601
 tar -xvjf cgen-20180601.tar.bz2 -C ./cgen-20180601
-rm cgen-1.1.tar.gz
+rm cgen-20180601.tar.bz2
